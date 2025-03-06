5d7b45f7ece706ecbea092a444894b9b995206ca
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-03-06 01:53:24 -0500

    update nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,26 +2,23 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
-import { HomeComponent } from './home/home.component';
-import { ScanMeComponent } from './scan-me/scan-me.component';
-import { SetUserInfoComponent } from './set-user-info/set-user-info.component';
-import { ContactListComponent } from './contact-list/contact-list.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    CommonModule,
-    RouterModule,
-    HomeComponent,
-    ScanMeComponent,
-    SetUserInfoComponent,
-    ContactListComponent,
-  ],
+  imports: [RouterOutlet, CommonModule, RouterModule],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Connect';
+  isNavbarCollapsed = true;
+
+  toggleNavbar() {
+    this.isNavbarCollapsed = !this.isNavbarCollapsed;
+  }
+
+  closeNavbar() {
+    this.isNavbarCollapsed = true;
+  }
 }

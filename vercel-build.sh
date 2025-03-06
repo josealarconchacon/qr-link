fb496f02faaeba21d7d3750f86bd87c759d97817
josealarconchacon <jose.alarcon.chacon@gmail.com>
2025-03-05 23:50:02 -0500

    test deploytment

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+echo "Installing dependencies..."
 npm install
+
+echo "Building the Angular app..."
 npm run build
-cp dist/qr-link/index.html dist/qr-link/404.html 
+
+echo "Setting up routing fallback..."
+cp dist/qr-link/index.html dist/qr-link/404.html
+
+echo "Build completed!" 
